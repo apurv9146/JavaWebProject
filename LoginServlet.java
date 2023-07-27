362634d8f035b5ee8bb9ad83fc5433832324f1b1
Apurva Gajanan Dikondawar <69241353+apurv9146@users.noreply.github.com>
2023-07-27 11:51:39 +0530

    Create LoginServlet.java

---
+++ LoginServlet.java
@@ -0,0 +1,29 @@
+// LoginServlet.java
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final String USERNAME = "user";
+    private static final String PASSWORD = "password";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        if (USERNAME.equals(username) && PASSWORD.equals(password)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("username", username);
+            response.sendRedirect("welcome.jsp");
+        } else {
+            response.sendRedirect("login.jsp");
+        }
+    }
+}
